8306bf9ae711b21388077db14a61034e58f7b4ad
Jalhematei <miriamlima2928i38@outlook.com>
2026-09-02 14:14:27 -0300

    fix: enforce domain-level 301 redirects in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'vozdaia.vercel.app' }],
+        destination: 'https://vozdaia.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'voz-da-ia.vercel.app' }],
+        destination: 'https://vozdaia.com/:path*',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
